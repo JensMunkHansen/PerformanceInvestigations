9eb79914ddb09188a98a1c44a296b430908bb6a4
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-02-22 13:52:59 +0100

    ..

---
+++ linux/spsMacros.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#if defined(__GNUC__)
+#define GCC_SPLIT_BLOCK(str) __asm__("//\n\t// " str "\n\t//\n");
+#elif defined(_WIN64)
+// Only 64-bit windows forbid inline assembly
+#define GCC_SPLIT_BLOCK(str)
+#else
+#define GCC_SPLIT_BLOCK(str) __asm {}
+#endif
