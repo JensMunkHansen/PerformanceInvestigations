1834177c96d47bbe419749adc61c186cabacf4fd
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-02-24 21:02:29 +0100

    aligned alloc

--- blocked/benchmark.cpp
+++ blocked/benchmark.cpp
@@ -11,7 +11,8 @@
 unsigned int numThreads = std::thread::hardware_concurrency();
 
 // Blocked serial implementation
-void blocked_mmul(const float *A, const float *B, float *C, std::size_t N) {
+void blocked_mmul(const float* A, const float* B, float* C, std::size_t N)
+{
   // For each row...
   for (std::size_t row = 0; row < N; row++)
     // For each block in the row...
@@ -24,14 +25,13 @@ void blocked_mmul(const float *A, const float *B, float *C, std::size_t N) {
           // Go through all the elements in the sub chunk
           for (std::size_t idx = 0; idx < 16; idx++)
             C[row * N + block + idx] +=
-                A[row * N + chunk + sub_chunk] *
-                B[chunk * N + sub_chunk * N + block + idx];
+              A[row * N + chunk + sub_chunk] * B[chunk * N + sub_chunk * N + block + idx];
 }
 
 // Blocked parallel implementation
-void blocked_parallel_mmul(const float *A, const float *B, float *C,
-                           std::size_t N, std::size_t start_row,
-                           std::size_t end_row) {
+void blocked_parallel_mmul(const float* A, const float* B, float* C, std::size_t N,
+  std::size_t start_row, std::size_t end_row)
+{
   // For each row...
   for (std::size_t row = start_row; row < end_row; row++)
     // For each block in the row...
@@ -44,12 +44,12 @@ void blocked_parallel_mmul(const float *A, const float *B, float *C,
           // Go through all the elements in the sub chunk
           for (std::size_t idx = 0; idx < 16; idx++)
             C[row * N + block + idx] +=
-                A[row * N + chunk + sub_chunk] *
-                B[chunk * N + sub_chunk * N + block + idx];
+              A[row * N + chunk + sub_chunk] * B[chunk * N + sub_chunk * N + block + idx];
 }
 
 // Blocked MMul benchmark
-static void blocked_mmul_bench(benchmark::State &s) {
+static void blocked_mmul_bench(benchmark::State& s)
+{
   // Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -59,9 +59,9 @@ static void blocked_mmul_bench(benchmark::State &s) {
   std::uniform_real_distribution<float> dist(-10, 10);
 
   // Create input matrices
-  float *A = new float[N * N];
-  float *B = new float[N * N];
-  float *C = new float[N * N];
+  float* A = new float[N * N];
+  float* B = new float[N * N];
+  float* C = new float[N * N];
 
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
@@ -69,7 +69,8 @@ static void blocked_mmul_bench(benchmark::State &s) {
   std::generate(C, C + N * N, [&] { return 0.0f; });
 
   // Main benchmark loop
-  for (auto _ : s) {
+  for (auto _ : s)
+  {
     blocked_mmul(A, B, C, N);
   }
 
@@ -79,13 +80,14 @@ static void blocked_mmul_bench(benchmark::State &s) {
   delete[] C;
 }
 BENCHMARK(blocked_mmul_bench)
-    ->Arg(2*numThreads*16)
-    ->Arg(4*numThreads*16)
-    ->Arg(6*numThreads*16)
-    ->Unit(benchmark::kMillisecond);
+  ->Arg(2 * numThreads * 16)
+  ->Arg(4 * numThreads * 16)
+  ->Arg(6 * numThreads * 16)
+  ->Unit(benchmark::kMillisecond);
 
 // Blocked MMul with aligned memory benchmark
-static void blocked_aligned_mmul_bench(benchmark::State &s) {
+static void blocked_aligned_mmul_bench(benchmark::State& s)
+{
   // Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -95,9 +97,9 @@ static void blocked_aligned_mmul_bench(benchmark::State &s) {
   std::uniform_real_distribution<float> dist(-10, 10);
 
   // Create input matrices
-  float *A = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *B = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *C = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
+  float* A = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
+  float* B = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
+  float* C = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
 
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
@@ -105,24 +107,26 @@ static void blocked_aligned_mmul_bench(benchmark::State &s) {
   std::generate(C, C + N * N, [&] { return 0.0f; });
 
   // Main benchmark loop
-  for (auto _ : s) {
+  for (auto _ : s)
+  {
     blocked_mmul(A, B, C, N);
   }
 
   // Free memory
-  free(A);
-  free(B);
-  free(C);
+  std::free(A);
+  std::free(B);
+  std::free(C);
 }
 BENCHMARK(blocked_aligned_mmul_bench)
-    ->Arg(2*numThreads*16)
-    ->Arg(4*numThreads*16)
-    ->Arg(6*numThreads*16)
-    ->Unit(benchmark::kMillisecond)
-    ->UseRealTime();
+  ->Arg(2 * numThreads * 16)
+  ->Arg(4 * numThreads * 16)
+  ->Arg(6 * numThreads * 16)
+  ->Unit(benchmark::kMillisecond)
+  ->UseRealTime();
 
 // Parallel blocked MMul benchmark
-static void parallel_blocked_mmul_bench(benchmark::State &s) {
+static void parallel_blocked_mmul_bench(benchmark::State& s)
+{
   // Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -132,9 +136,9 @@ static void parallel_blocked_mmul_bench(benchmark::State &s) {
   std::uniform_real_distribution<float> dist(-10, 10);
 
   // Create input matrices
-  float *A = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *B = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *C = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
+  float* A = static_cast<float*>(aligned_alloc(64, N * N * sizeof(float)));
+  float* B = static_cast<float*>(aligned_alloc(64, N * N * sizeof(float)));
+  float* C = static_cast<float*>(aligned_alloc(64, N * N * sizeof(float)));
 
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
@@ -151,18 +155,20 @@ static void parallel_blocked_mmul_bench(benchmark::State &s) {
   std::size_t n_rows = N / num_threads;
 
   // Main benchmark loop
-  for (auto _ : s) {
+  for (auto _ : s)
+  {
     // Launch threads
     std::size_t end_row = 0;
-    for (std::size_t i = 0; i < num_threads; i++) {
+    for (std::size_t i = 0; i < num_threads; i++)
+    {
       auto start_row = i * n_rows;
       end_row = start_row + n_rows;
-      threads.emplace_back(
-          [&] { blocked_parallel_mmul(A, B, C, N, start_row, end_row); });
+      threads.emplace_back([&] { blocked_parallel_mmul(A, B, C, N, start_row, end_row); });
     }
 
     // Wait for all threads to complete
-    for (auto &t : threads) t.join();
+    for (auto& t : threads)
+      t.join();
 
     // Clear the threads each iteration of the benchmark
     threads.clear();
@@ -174,30 +180,37 @@ static void parallel_blocked_mmul_bench(benchmark::State &s) {
   free(C);
 }
 BENCHMARK(parallel_blocked_mmul_bench)
-    ->Arg(2*numThreads*16)
-    ->Arg(4*numThreads*16)
-    ->Arg(6*numThreads*16)
-    ->Unit(benchmark::kMillisecond)
-    ->UseRealTime();
+  ->Arg(2 * numThreads * 16)
+  ->Arg(4 * numThreads * 16)
+  ->Arg(6 * numThreads * 16)
+  ->Unit(benchmark::kMillisecond)
+  ->UseRealTime();
 
-int main(int argc, char** argv) {
-    // Separate user arguments and benchmark arguments
-    std::vector<char*> benchmark_args;
-    for (int i = 0; i < argc; ++i) {
-        if (std::string(argv[i]).find("--") == 0 || i == 0) {
-            // Keep benchmark-specific arguments (starting with '--') and the program name
-            benchmark_args.push_back(argv[i]);
-        } else {
-            // Custom user arguments
-          numThreads = std::min(static_cast<unsigned int>(std::stoi(argv[i])), std::thread::hardware_concurrency());
-        }
+int main(int argc, char** argv)
+{
+  // Separate user arguments and benchmark arguments
+  std::vector<char*> benchmark_args;
+  for (int i = 0; i < argc; ++i)
+  {
+    if (std::string(argv[i]).find("--") == 0 || i == 0)
+    {
+      // Keep benchmark-specific arguments (starting with '--') and the program name
+      benchmark_args.push_back(argv[i]);
     }
-    // Pass filtered arguments to Google Benchmark
-    int benchmark_argc = static_cast<int>(benchmark_args.size());
-    char** benchmark_argv = benchmark_args.data();
+    else
+    {
+      // Custom user arguments
+      numThreads = std::min(
+        static_cast<unsigned int>(std::stoi(argv[i])), std::thread::hardware_concurrency());
+    }
+  }
+  // Pass filtered arguments to Google Benchmark
+  int benchmark_argc = static_cast<int>(benchmark_args.size());
+  char** benchmark_argv = benchmark_args.data();
 
-    benchmark::Initialize(&benchmark_argc, benchmark_argv);
-    if (benchmark::ReportUnrecognizedArguments(benchmark_argc, benchmark_argv)) return 1;
-    benchmark::RunSpecifiedBenchmarks();
-    return 0;
+  benchmark::Initialize(&benchmark_argc, benchmark_argv);
+  if (benchmark::ReportUnrecognizedArguments(benchmark_argc, benchmark_argv))
+    return 1;
+  benchmark::RunSpecifiedBenchmarks();
+  return 0;
 }
--- blocked_column/benchmark.cpp
+++ blocked_column/benchmark.cpp
@@ -5,16 +5,16 @@
 #include <algorithm>
 #include <atomic>
 #include <cstdlib>
+#include <iostream>
 #include <random>
 #include <thread>
 #include <vector>
-#include <iostream>
 
 unsigned int numThreads = std::thread::hardware_concurrency();
 
 // Blocked column serial implementation
-void blocked_column_mmul(const float *A, const float *B, float *C,
-                         std::size_t N) {
+void blocked_column_mmul(const float* A, const float* B, float* C, std::size_t N)
+{
   // For each chunk of columns
   for (std::size_t col_chunk = 0; col_chunk < N; col_chunk += 16)
     // For each row in that chunk of columns...
@@ -27,14 +27,13 @@ void blocked_column_mmul(const float *A, const float *B, float *C,
           // Solve for each element in this tile row
           for (std::size_t idx = 0; idx < 16; idx++)
             C[row * N + col_chunk + idx] +=
-                A[row * N + tile + tile_row] *
-                B[tile * N + tile_row * N + col_chunk + idx];
+              A[row * N + tile + tile_row] * B[tile * N + tile_row * N + col_chunk + idx];
 }
 
 // Blocked serial implementation
-void blocked_column_parallel_mmul(const float *A, const float *B, float *C,
-                                  std::size_t N, std::size_t start_col,
-                                  std::size_t end_col) {
+void blocked_column_parallel_mmul(const float* A, const float* B, float* C, std::size_t N,
+  std::size_t start_col, std::size_t end_col)
+{
   for (auto col_chunk = start_col; col_chunk < end_col; col_chunk += 16)
     // For each row in that chunk of columns...
     for (std::size_t row = 0; row < N; row++)
@@ -46,12 +45,12 @@ void blocked_column_parallel_mmul(const float *A, const float *B, float *C,
           // Solve for each element in this tile row
           for (std::size_t idx = 0; idx < 16; idx++)
             C[row * N + col_chunk + idx] +=
-                A[row * N + tile + tile_row] *
-                B[tile * N + tile_row * N + col_chunk + idx];
+              A[row * N + tile + tile_row] * B[tile * N + tile_row * N + col_chunk + idx];
 }
 
 // Blocked column MMul with aligned memory benchmark
-static void blocked_column_aligned_mmul_bench(benchmark::State &s) {
+static void blocked_column_aligned_mmul_bench(benchmark::State& s)
+{
   // Number Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -61,9 +60,9 @@ static void blocked_column_aligned_mmul_bench(benchmark::State &s) {
   std::uniform_real_distribution<float> dist(-10, 10);
 
   // Create input matrices
-  float *A = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *B = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *C = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
+  float* A = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
+  float* B = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
+  float* C = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
 
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
@@ -71,23 +70,25 @@ static void blocked_column_aligned_mmul_bench(benchmark::State &s) {
   std::generate(C, C + N * N, [&] { return 0.0f; });
 
   // Main benchmark loop
-  for (auto _ : s) {
+  for (auto _ : s)
+  {
     blocked_column_mmul(A, B, C, N);
   }
 
   // Free memory
-  free(A);
-  free(B);
-  free(C);
+  std::free(A);
+  std::free(B);
+  std::free(C);
 }
 BENCHMARK(blocked_column_aligned_mmul_bench)
-    ->Arg(2*numThreads*16)
-    ->Arg(4*numThreads*16)
-    ->Arg(6*numThreads*16)
-    ->Unit(benchmark::kMillisecond);
+  ->Arg(2 * numThreads * 16)
+  ->Arg(4 * numThreads * 16)
+  ->Arg(6 * numThreads * 16)
+  ->Unit(benchmark::kMillisecond);
 
 // Blocked column MMul with aligned memory benchmark
-static void blocked_column_mmul_bench(benchmark::State &s) {
+static void blocked_column_mmul_bench(benchmark::State& s)
+{
   // Number Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -97,9 +98,9 @@ static void blocked_column_mmul_bench(benchmark::State &s) {
   std::uniform_real_distribution<float> dist(-10, 10);
 
   // Create input matrices
-  float *A = static_cast<float *>(malloc(N * N * sizeof(float)));
-  float *B = static_cast<float *>(malloc(N * N * sizeof(float)));
-  float *C = static_cast<float *>(malloc(N * N * sizeof(float)));
+  float* A = static_cast<float*>(malloc(N * N * sizeof(float)));
+  float* B = static_cast<float*>(malloc(N * N * sizeof(float)));
+  float* C = static_cast<float*>(malloc(N * N * sizeof(float)));
 
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
@@ -107,7 +108,8 @@ static void blocked_column_mmul_bench(benchmark::State &s) {
   std::generate(C, C + N * N, [&] { return 0.0f; });
 
   // Main benchmark loop
-  for (auto _ : s) {
+  for (auto _ : s)
+  {
     blocked_column_mmul(A, B, C, N);
   }
 
@@ -117,14 +119,14 @@ static void blocked_column_mmul_bench(benchmark::State &s) {
   free(C);
 }
 BENCHMARK(blocked_column_mmul_bench)
-    ->Arg(2*numThreads*16)
-    ->Arg(4*numThreads*16)
-    ->Arg(6*numThreads*16)
-    ->Unit(benchmark::kMillisecond);
-
+  ->Arg(2 * numThreads * 16)
+  ->Arg(4 * numThreads * 16)
+  ->Arg(6 * numThreads * 16)
+  ->Unit(benchmark::kMillisecond);
 
 // Parallel MMul benchmark
-static void parallel_blocked_column_mmul_bench(benchmark::State &s) {
+static void parallel_blocked_column_mmul_bench(benchmark::State& s)
+{
   // Number Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -134,9 +136,9 @@ static void parallel_blocked_column_mmul_bench(benchmark::State &s) {
   std::uniform_real_distribution<float> dist(-10, 10);
 
   // Create input matrices
-  float *A = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *B = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
-  float *C = static_cast<float *>(aligned_alloc(64, N * N * sizeof(float)));
+  float* A = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
+  float* B = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
+  float* C = static_cast<float*>(std::aligned_alloc(64, N * N * sizeof(float)));
 
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
@@ -153,57 +155,65 @@ static void parallel_blocked_column_mmul_bench(benchmark::State &s) {
   std::size_t n_cols = N / num_threads;
 
   // Main benchmark loop
-  for (auto _ : s) {
+  for (auto _ : s)
+  {
     // Launch threads
     std::size_t start_col = 0;
-    for (std::size_t i = 0; i < num_threads; i++) {
+    for (std::size_t i = 0; i < num_threads; i++)
+    {
       auto end_col = start_col + n_cols;
-      threads.emplace_back([&] {
-        blocked_column_parallel_mmul(A, B, C, N, start_col, end_col);
-      });
+      threads.emplace_back([&] { blocked_column_parallel_mmul(A, B, C, N, start_col, end_col); });
       start_col += n_cols;
     }
 
     // Wait for all threads to complete
-    for (auto &t : threads) t.join();
+    for (auto& t : threads)
+      t.join();
 
     // Clear the threads each iteration of the benchmark
     threads.clear();
   }
 
   // Free memory
-  free(A);
-  free(B);
-  free(C);
+  std::free(A);
+  std::free(B);
+  std::free(C);
 }
 BENCHMARK(parallel_blocked_column_mmul_bench)
-    ->Arg(2*16 * numThreads) // Not good always 16
-    ->Arg(4*16 * numThreads)
-    ->Arg(6*16 * numThreads)
-    ->Unit(benchmark::kMillisecond)
-    ->UseRealTime();
+  ->Arg(2 * 16 * numThreads) // Not good always 16
+  ->Arg(4 * 16 * numThreads)
+  ->Arg(6 * 16 * numThreads)
+  ->Unit(benchmark::kMillisecond)
+  ->UseRealTime();
 
-int main(int argc, char** argv) {
-    // Separate user arguments and benchmark arguments
-    std::vector<char*> benchmark_args;
-    for (int i = 0; i < argc; ++i) {
-        if (std::string(argv[i]).find("--") == 0 || i == 0) {
-            // Keep benchmark-specific arguments (starting with '--') and the program name
-            benchmark_args.push_back(argv[i]);
-        } else {
-            // Custom user arguments
-          numThreads = std::min(static_cast<unsigned int>(std::stoi(argv[i])), std::thread::hardware_concurrency());
-          std::cout << numThreads << std::endl;
-        }
+int main(int argc, char** argv)
+{
+  // Separate user arguments and benchmark arguments
+  std::vector<char*> benchmark_args;
+  for (int i = 0; i < argc; ++i)
+  {
+    if (std::string(argv[i]).find("--") == 0 || i == 0)
+    {
+      // Keep benchmark-specific arguments (starting with '--') and the program name
+      benchmark_args.push_back(argv[i]);
     }
-    // Pass filtered arguments to Google Benchmark
-    int benchmark_argc = static_cast<int>(benchmark_args.size());
-    char** benchmark_argv = benchmark_args.data();
+    else
+    {
+      // Custom user arguments
+      numThreads = std::min(
+        static_cast<unsigned int>(std::stoi(argv[i])), std::thread::hardware_concurrency());
+      std::cout << numThreads << std::endl;
+    }
+  }
+  // Pass filtered arguments to Google Benchmark
+  int benchmark_argc = static_cast<int>(benchmark_args.size());
+  char** benchmark_argv = benchmark_args.data();
 
-    benchmark::Initialize(&benchmark_argc, benchmark_argv);
-    if (benchmark::ReportUnrecognizedArguments(benchmark_argc, benchmark_argv)) return 1;
-    benchmark::RunSpecifiedBenchmarks();
-    return 0;
+  benchmark::Initialize(&benchmark_argc, benchmark_argv);
+  if (benchmark::ReportUnrecognizedArguments(benchmark_argc, benchmark_argv))
+    return 1;
+  benchmark::RunSpecifiedBenchmarks();
+  return 0;
 }
 
 // BENCHMARK_MAIN();
