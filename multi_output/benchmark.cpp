1840681cd078c3cf3677d9fa496c066c00e54016
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-04-28 22:15:24 +0200

    Manual vectorization hints for MSVC

--- multi_output/benchmark.cpp
+++ multi_output/benchmark.cpp
@@ -19,23 +19,24 @@ unsigned int numThreads = std::thread::hardware_concurrency();
 // Blocked column multi-output serial implementation
 void blocked_column_multi_output_mmul(const float* A, const float* B, float* C, std::size_t N)
 {
-  // No effect
-#pragma clang loop vectorize(enable)
-#pragma clang loop interleave(enable)
-#pragma clang loop unroll(enable)
-
   // For each chunk of columns
+#pragma loop(ivdep)
   for (std::size_t col_chunk = 0; col_chunk < N; col_chunk += 16)
-    // For each chunk of rows
+  // For each chunk of rows
+#pragma loop(ivdep)
     for (std::size_t row_chunk = 0; row_chunk < N; row_chunk += 16)
-      // For each block of elements in this row of this column chunk
-      // Solve for 16 elements at a time
+    // For each block of elements in this row of this column chunk
+    // Solve for 16 elements at a time
+#pragma loop(ivdep)
       for (std::size_t tile = 0; tile < N; tile += 16)
-        // Apply that tile to each row of the row chunk
+      // Apply that tile to each row of the row chunk
+#pragma loop(ivdep)
         for (std::size_t row = 0; row < 16; row++)
-          // For each row in the tile
+        // For each row in the tile
+#pragma loop(ivdep)
           for (std::size_t tile_row = 0; tile_row < 16; tile_row++)
-            // Solve for each element in this tile row
+          // Solve for each element in this tile row
+#pragma loop(ivdep)
             for (std::size_t idx = 0; idx < 16; idx++)
               C[(row + row_chunk) * N + col_chunk + idx] +=
                 A[(row + row_chunk) * N + tile + tile_row] *
@@ -47,38 +48,27 @@ void blocked_column_multi_output_parallel_mmul(const float* A, const float* B, f
   std::size_t N, std::size_t start_col, std::size_t end_col)
 {
   // For each chunk of columns
+#pragma loop(ivdep)
   for (std::size_t col_chunk = start_col; col_chunk < end_col; col_chunk += 16)
-    // For each chunk of rows
+  // For each chunk of rows
+#pragma loop(ivdep)
     for (std::size_t row_chunk = 0; row_chunk < N; row_chunk += 16)
-      // For each block of elements in this row of this column chunk
-      // Solve for 16 elements at a time
+    // For each block of elements in this row of this column chunk
+    // Solve for 16 elements at a time
+#pragma loop(ivdep)
       for (std::size_t tile = 0; tile < N; tile += 16)
-        // For apply that tile to each row of the row chunk
+      // For apply that tile to each row of the row chunk
+#pragma loop(ivdep)
         for (std::size_t row = 0; row < 16; row++)
-          // For each row in the tile
+        // For each row in the tile
+#pragma loop(ivdep)
           for (std::size_t tile_row = 0; tile_row < 16; tile_row++)
-            // Solve for each element in this tile row
+          // Solve for each element in this tile row
+#pragma loop(ivdep)
             for (std::size_t idx = 0; idx < 16; idx++)
               C[(row + row_chunk) * N + col_chunk + idx] +=
                 A[(row + row_chunk) * N + tile + tile_row] *
                 B[tile * N + tile_row * N + col_chunk + idx];
-}
-
-void blocked_column_parallel_atomic_gemm(
-  const double* A, const double* B, double* C, std::size_t N, std::atomic<uint64_t>& pos)
-{
-  for (auto col_chunk = pos.fetch_add(16); col_chunk < N; col_chunk = pos.fetch_add(16))
-    // For each row in that chunk of columns...
-    for (std::size_t row = 0; row < N; row++)
-      // For each block of elements in this row of this column chunk
-      // Solve for 16 elements at a time
-      for (std::size_t tile = 0; tile < N; tile += 16)
-        // For each row in the tile
-        for (std::size_t tile_row = 0; tile_row < 16; tile_row++)
-          // Solve for each element in this tile row
-          for (std::size_t idx = 0; idx < 16; idx++)
-            C[row * N + col_chunk + idx] +=
-              A[row * N + tile + tile_row] * B[tile * N + tile_row * N + col_chunk + idx];
 }
 
 // Blocked column multi-output MMul with aligned memory benchmark
