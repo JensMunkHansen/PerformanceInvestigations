cd79cc62bbe4a198b81d19c5ec1c1a1f3c490100
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-04-28 22:39:07 +0200

    scalar good - parallel bad

--- run_gcc.sh
+++ run_gcc.sh
@@ -11,4 +11,5 @@ exec > "$output_file" 2>&1
 ./build/linux-gcc/bin/Release/blocked --benchmark_min_time=3.0s
 ./build/linux-gcc/bin/Release/blocked_column --benchmark_min_time=3.0s
 ./build/linux-gcc/bin/Release/blocked_column_multi_output --benchmark_min_time=3.0s
-./build/linux-gcc/bin/Release/mmul_bench --benchmark_min_time=3.0s
+./build/linux-gcc/bin/Release/blocked_column_multi_output_accumulate --benchmark_min_time=3.0s
+./build/linux-gcc/bin/Release/intelmkl --benchmark_min_time=3.0s
